0fcdf0e699abf68d51fabf13a339b532f7dc75de
MarlonDSC <marlonsubuyu.2012@gmail.com>
2023-07-29 13:49:53 -0400

    🔨 add: Add tabs for OneUIBottomNavigationBar

---
+++ lib/ui/pages/timer_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class TimerPage extends StatelessWidget {
+  final String title;
+  const TimerPage({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text('Timer');
+  }
+}
---
+++ lib/ui/pages/world_clock_page.dart
@@ -0,0 +1,48 @@
+import 'package:atomic_design/ui/one_ui.dart';
+
+class WorldclockPage extends StatelessWidget {
+  final String title;
+  const WorldclockPage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return OneUINestedScrollView(
+      expandedWidget: Text(
+        title,
+        style: const TextStyle(fontSize: 30),
+      ),
+      collapsedWidget: Text(
+        title,
+        style: const TextStyle(fontSize: 20),
+      ),
+      boxDecoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      leadingIcon: IconButton(
+        onPressed: () => {},
+        icon: SvgPicture.asset(OneUIIcons.drawer),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(OneUIIcons.more),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(OneUIIcons.search),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: SvgPicture.asset(OneUIIcons.abAdd),
+        ),
+      ],
+      sliverList: SliverList(
+          delegate: SliverChildBuilderDelegate(
+        (context, index) => ListTile(
+          leading: SvgPicture.asset(OneUIIcons.contact),
+          title: Text('Contact $index'),
+        ),
+      )),
+    );
+  }
+}
