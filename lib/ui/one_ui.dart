39edc43952e95d7035c647feb57b51738ffc6814
MarlonDSC <marlonsubuyu.2012@gmail.com>
2023-09-02 20:52:45 -0400

    🔨 update: format and add new exports

--- lib/ui/one_ui.dart
+++ lib/ui/one_ui.dart
@@ -1,21 +1,25 @@
 library one_ui;
 
 //atoms
-export 'package:atomic_design/ui/atoms/one_ui_bottom_navigation_item.dart';
-export 'package:atomic_design/ui/atoms/tab_item.dart';
+export 'atoms/one_ui_bottom_navigation_item.dart';
+export 'atoms/tab_item.dart';
+export 'atoms/one_ui_container.dart';
+export 'atoms/one_ui_switch.dart';
 
 //foundations
-export 'package:atomic_design/ui/foundations/icons.dart';
+export 'foundations/tokens/icons.dart';
+export 'foundations/tokens/palette.dart';
+export 'foundations/tokens/colors.dart';
 
 //organisms
-export 'package:atomic_design/ui/organisms/one_ui_bottom_navigation_bar.dart';
-export 'package:atomic_design/ui/organisms/one_ui_nested_scroll_view.dart';
+export 'organisms/one_ui_bottom_navigation_bar.dart';
+export 'organisms/one_ui_nested_scroll_view.dart';
 
 //pages
-export 'package:atomic_design/ui/pages/alarm_page.dart';
-export 'package:atomic_design/ui/pages/stopwatch_page.dart';
-export 'package:atomic_design/ui/pages/timer_page.dart';
-export 'package:atomic_design/ui/pages/world_clock_page.dart';
+export 'pages/alarm_page.dart';
+export 'pages/world_clock_page.dart';
+export 'pages/stopwatch_page.dart';
+export 'pages/timer_page.dart';
 
 //Third-party
 export 'package:flutter/material.dart';
