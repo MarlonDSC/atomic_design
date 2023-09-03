8a5848e7681e0cd0f45d3f66f113d31e7d6ff796
MarlonDSC <marlonsubuyu.2012@gmail.com>
2023-09-02 20:56:54 -0400

    🔧 config: add palette and light theme

---
+++ lib/ui/foundations/tokens/palette.dart
@@ -0,0 +1,47 @@
+import 'package:atomic_design/ui/foundations/tokens/colors.dart';
+
+abstract class Palette {
+  final ScaffoldColors scaffoldColors;
+  final SwitchColors switchColors;
+  final TextColors textColors;
+  final CardColors cardColors;
+  final ButtonColors buttonColors;
+
+  Palette({
+    required this.scaffoldColors,
+    required this.switchColors,
+    required this.textColors,
+    required this.cardColors,
+    required this.buttonColors,
+  });
+}
+
+enum PaletteType { light }
+
+Map<PaletteType, Palette> palettes = {
+  PaletteType.light: lightPalette,
+};
+
+Palette lightPalette = LightPalette(
+  scaffoldColors: ScaffoldColors(
+    primary: AppColors.kBlueLotus,
+    background: AppColors.kWhiteSmoke,
+  ),
+  buttonColors: ButtonColors(
+    background: AppColors.kBlueLotus,
+    textColor: AppColors.kAquaHaze,
+  ),
+  cardColors: CardColors(
+    background: AppColors.kSoapstone,
+  ),
+  switchColors: SwitchColors(
+    enabled: AppColors.kBlueLotus,
+    disabled: AppColors.kLemonGrass,
+  ),
+  textColors: TextColors(
+    disabled: AppColors.kGrayCloud,
+    error: AppColors.kPersianRed,
+    primary: AppColors.kBlueLotus,
+    secondary: AppColors.kCloudyGray,
+  ),
+);
